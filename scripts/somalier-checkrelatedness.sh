374dd56b470c1bf63cbcdf6a9da994a743af783e
Laurel Hiatt <u1264408@redwood10.int.chpc.utah.edu>
2024-07-18 15:10:57 -0600

    adding the other colon samples, making fastp more stringent

--- scripts/somalier-checkrelatedness.sh
+++ scripts/somalier-checkrelatedness.sh
@@ -3,5 +3,5 @@
 input_dir='/uufs/chpc.utah.edu/common/HIPAA/u1264408/u1264408/Git/SEMIColon/data/output/'
 groups='/uufs/chpc.utah.edu/common/HIPAA/u1264408/u1264408/Git/SEMIColon/data/somalier-groups.txt'
 
-/uufs/chpc.utah.edu/common/HIPAA/u1264408/tools/somalier/somalier relate ${input_dir}/*.somalier -g $groups -o groups
-#/uufs/chpc.utah.edu/common/HIPAA/u1264408/tools/somalier/somalier relate ${input_dir}/*.somalier  -o nogroups
+# /uufs/chpc.utah.edu/common/HIPAA/u1264408/tools/somalier/somalier relate ${input_dir}/*.somalier -g $groups -o groups
+/uufs/chpc.utah.edu/common/HIPAA/u1264408/tools/somalier/somalier relate ${input_dir}/*.somalier  -o nogroups
