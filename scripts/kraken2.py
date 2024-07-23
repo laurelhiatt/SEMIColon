5fb7ac2106eb08e81c971065cc40db0567352239
Laurel Hiatt <u1264408@father.int.chpc.utah.edu>
2024-07-23 12:15:34 -0600

    kraken2 to view species contam

--- scripts/kraken2.py
+++ scripts/kraken2.py
@@ -28,28 +28,31 @@ def run_kraken2(input_fastq1, input_fastq2, db_path, output_report, output_class
         raise subprocess.CalledProcessError(process.returncode, command)
 
 def find_paired_fastq_files(directory):
-    fastq_files = glob.glob(os.path.join(directory, "*.fastq"))
+    fastq_files = glob.glob(os.path.join(directory, "*.fastq.gz"))
     paired_files = {}
 
     for file in fastq_files:
         base_name = os.path.basename(file)
         print(base_name)
-        if "_R1" in base_name:
-            pair = base_name.replace("_R1", "_R2")
+        if "R1" in base_name:
+            pair = base_name.replace("R1", "R2")
             if os.path.join(directory, pair) in fastq_files:
                 paired_files[file] = os.path.join(directory, pair)
 
     return paired_files
 
 def main():
-    directory = "/uufs/chpc.utah.edu/common/HIPAA/u1264408/u1264408/Git/SEMIColon/data/output/cryptdata/fastq/"
+    #directory = "/uufs/chpc.utah.edu/common/HIPAA/u1264408/u1264408/Git/SEMIColon/data/output/cryptdata/fastq/"
+    directory = "/scratch/ucgd/lustre-labs/quinlan/data-shared/SEMIColon/24286R/24286R/Fastq/shortname_fastqs/"
+    print(directory)
     db_path = "/uufs/chpc.utah.edu/common/HIPAA/u1264408/u1264408/Git/SEMIColon/data/standard_db/"
-
+    print(db_path)
 
     paired_files = find_paired_fastq_files(directory)
+    print(paired_files)
 
     for input_fastq1, input_fastq2 in paired_files.items():
-        base_name = os.path.basename(input_fastq1).replace("_R1_", "")
+        base_name = os.path.basename(input_fastq1).replace("R1", "")
         output_report = "{}_kraken2_report.txt".format(base_name)
         output_classification = "{}_kraken2_classification.txt".format(base_name)
 
