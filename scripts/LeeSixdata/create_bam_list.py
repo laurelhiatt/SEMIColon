2a083ce18142a200ba2dd71b61996ea0e711c33c
laurelhiatt <laurel.hiatt@gmail.com>
2024-09-19 10:07:56 -0600

    THIS WORKED <3

--- scripts/LeeSixdata/create_bam_list.py
+++ scripts/LeeSixdata/create_bam_list.py
@@ -6,8 +6,10 @@ donor = sys.argv[1]
 location = sys.argv[2]
 out_dir = sys.argv[3]
 matches = {
-    'HLS': {'crypt_samples': ['HLS_1C_30_B5']},
-    'PD34199': {'crypt_samples': ['PD34199a_t28']},
+    'HLS': {'crypt_samples': ['HLS_1C_30_B5', 'HLS_1C_30_D5', 'HLS_1C_30_G5', 'HLS_1C_30_H5', 'HLS_2C_30_D6', 'HLS_2C_30_E6']},
+    'PD28690': {'crypt_samples': ['PD28690bx_2_a5', 'PD28690bx_2_d5', 'PD28690cb_2_g3', 'PD28690cb_2_h3']},
+    'PD34199': {'crypt_samples': ['PD34199a_c17', 'PD34199a_c22', 'PD34199a_c27', 'PD34199a_c28', 'PD34199a_c31', 'PD34199a_c3', 'PD34199a_c5', 'PD34199a_c7', 'PD34199a_t31', 'PD34199a_t28']},
+    'PD34202': {'crypt_samples': ['PD34202a_s9']}
     }
 
 crypt_samples = matches[donor]["crypt_samples"]
