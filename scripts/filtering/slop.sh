f2b09a129ac17f87ebe3c8677e5f2885cfc1b119
laurelhiatt <laurel.hiatt@gmail.com>
2025-09-12 15:54:18 -0600

    final working script for slop analyses

--- scripts/filtering/slop.sh
+++ scripts/filtering/slop.sh
@@ -1,32 +1,29 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# ==== USER VARIABLES ====
 LCRBED="/scratch/ucgd/lustre-labs/quinlan/data-shared/annotations/LCR-hs38.bed.gz"
 SRBED="/scratch/ucgd/lustre-labs/quinlan/data-shared/annotations/GRCh38.UCSC.SimpleRepeats.simple.bed"
 GENOME="/uufs/chpc.utah.edu/common/HIPAA/u1264408/u1264408/Git/SEMIColon/genome.chrom.sizes"                     # chrom sizes file
-VCFDIR="/uufs/chpc.utah.edu/common/HIPAA/u1264408/u1264408/Git/SEMIColon/data/output/CellCut/results/GB115"                           # directory of .vcf.gz files
-DISTANCES=(100 300 500)
-BEDS=("LCR" "SR")                          # flank sizes to check
+VCFDIR="/uufs/chpc.utah.edu/common/HIPAA/u1264408/u1264408/Git/SEMIColon/data/output/CellCut/results/vaf_spectra_under885"                           # directory of .vcf.gz files
+DISTANCES=(100 300 500)                       # flank sizes to check
 
-# ==== PREP OUTPUT DIRS ====
 for d in "${DISTANCES[@]}"; do
     mkdir -p results_${d}bp
 done
 
-# ==== MAKE LCR FLANKS ====
+# LCR slops
 for d in "${DISTANCES[@]}"; do
-    bedtools flank -i "$REFBED" -g "$GENOME" -l "$d" -r "$d" > flanks_${d}bp.bed
+    bedtools slop -i "$LCRBED" -g "$GENOME" -b "$d" > LCR_slops_${d}bp.bed
 done
 
-# ==== MAKE LCR FLANKS ====
+# simple repeat slops
 for d in "${DISTANCES[@]}"; do
-    bedtools flank -i "$REFBED" -g "$GENOME" -l "$d" -r "$d" > flanks_${d}bp.bed
+    bedtools slop -i "$SRBED" -g "$GENOME" -b "$d" > SR_slops_${d}bp.bed
 done
 
-# ==== LOOP THROUGH VCFs ====
-for vcf in "${VCFDIR}"/*snvs.vcf.gz; do
-    sample=$(basename "$vcf" .vcf.gz)
+# loo pthrough vcf
+for vcf in "${VCFDIR}"/*snvs.vcf; do
+    sample=$(basename "$vcf" .vcf)
 
     # Convert VCF to BED (0-based start, end = POS if END missing)
     tmpbed=$(mktemp)
@@ -35,9 +32,13 @@ for vcf in "${VCFDIR}"/*snvs.vcf.gz; do
 
     for d in "${DISTANCES[@]}"; do
         # Find closest variant to each flank interval
-        bedtools closest -a {bed}_flanks_${d}bp.bed -b "$tmpbed" -d \
+        bedtools intersect -a LCR_slops_${d}bp.bed -b "$tmpbed" \
         | awk -v maxd="$d" '$NF <= maxd {print}' \
-        > results_${d}bp/${sample}_closest_${d}bp.tsv
+        > results_${d}bp/${sample}_LCRoverlap_${d}bp.tsv
+
+        bedtools intersect -a SR_slops_${d}bp.bed -b "$tmpbed" \
+        | awk -v maxd="$d" '$NF <= maxd {print}' \
+        > results_${d}bp/${sample}_SRoverlap_${d}bp.tsv
     done
 
     rm "$tmpbed"
